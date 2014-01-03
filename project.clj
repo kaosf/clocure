dbdb5293548c0f071e9776c63fceee962ab3481e
ka <ka.kaosf@gmail.com>
2014-01-04 00:45:18 +0900

    modify the url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clocure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/kaosf/clocure"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
