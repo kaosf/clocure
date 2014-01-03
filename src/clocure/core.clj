dbbbffa023b4c98b2f14e893da5528b49910dfcd
ka <ka.kaosf@gmail.com>
2014-01-03 16:53:07 +0900

    implement Human and Precure

--- src/clocure/core.clj
+++ src/clocure/core.clj
@@ -1,6 +1,42 @@
 (ns clocure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Human
+  [id
+   name
+   first-name      family-name
+   first-name-kana family-name-kana
+   first-name-en   family-name-en
+   after-transforming])
+
+(defmacro def-human
+  [first-name-en   family-name-en
+   first-name      family-name
+   first-name-kana family-name-kana
+   & after-transforming]
+  `(intern *ns*
+    '~first-name-en ; use as an ID
+    (->Human
+      '~first-name-en ; use as an ID
+      (str '~family-name '~first-name)
+      (str '~first-name)      (str '~family-name)
+      (str '~first-name-kana) (str '~family-name-kana)
+      (clojure.string/capitalize '~first-name-en)
+      (clojure.string/capitalize '~family-name-en)
+      '~after-transforming)))
+
+(defrecord Precure
+  [id
+   name    short-name
+   name-en short-name-en
+   before])
+
+(defmacro def-precure
+  [short-name-en short-name before]
+  `(intern *ns*
+    '~short-name-en ; use as an ID
+    (->Precure
+      '~short-name-en ; use as an ID
+      (str "キュア" '~short-name) (str '~short-name)
+      (str "Cure " (clojure.string/capitalize '~short-name-en))
+      (clojure.string/capitalize '~short-name-en)
+      '~before)))
--- test/clocure/core_test.clj
+++ test/clocure/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [clocure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest human-test
+  (let
+    [human (->Human
+             'nagisa
+             "美墨なぎさ"
+             "なぎさ" "美墨"
+             "なぎさ" "みすみ"
+             "nagisa" "misumi"
+             '(black))]
+    (testing "Human"
+      (is (=
+        '(:id
+          :name
+          :first-name      :family-name
+          :first-name-kana :family-name-kana
+          :first-name-en   :family-name-en
+          :after-transforming)
+        (keys human))))))
