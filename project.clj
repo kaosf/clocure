cdbd79d7fcb99511ae88f188278772673760e23e
ka <ka.kaosf@gmail.com>
2014-08-23 18:01:11 +0900

    Modify project.clj for dployment to Clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://github.com/kaosf/clocure"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :signing {:gpg-key "ka.kaosf@gmail.com"}
+  :deploy-repositories
+    [["releases" {:url "https://github.com/kaosf/clocure"
+                  :signing {:gpg-key "93535FB2"}}]
+     ["snapshots" "https://github.com/kaosf/clocure/tags"]])
