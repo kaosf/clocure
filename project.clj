a48e7953db74ec3433d0b7b615f5416d197aa14a
ka <ka.kaosf@gmail.com>
2015-03-11 21:28:08 +0900

    Enable auto test
    Run "lein autoexpect" for auto test.
    
    ref.
      http://dev.classmethod.jp/client-side/language-client-side/lein-autoexpect/
      https://github.com/jakemcc/lein-autoexpect

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]]
+  :plugin [[lein-autoexpect "1.4.2"]]
+  :profiles {:dev {:dependencies [[expectations "2.0.9"]]}}
   :signing {:gpg-key "ka.kaosf@gmail.com"}
   :deploy-repositories
     [["releases" {:url "https://github.com/kaosf/clocure"
